d448f43a546d53dc56e675f8a4339f2cfaffe714
sajibsaha4647 <sksaha4648@gmail.com>
2022-05-27 23:27:33 +0600

    updated

--- app/src/main/java/com/example/customdatabindingadapters/MainActivity.kt
+++ app/src/main/java/com/example/customdatabindingadapters/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.customdatabindingadapters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.customdatabindingadapters.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val post = Post("introduction to kotlin","this all about my channel discription","http://drive.google.com/uc?id=1pZqz5HDU7x4BF7Ye3dQYUjJ7mCaMwTyf")
+
+        binding.post = post
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/customdatabindingadapters/Post.kt
@@ -0,0 +1,3 @@
+package com.example.customdatabindingadapters
+
+data class Post(val title:String,val discription:String,val url:String)
